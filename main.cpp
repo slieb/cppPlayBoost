fd7beac563bcef61129642d216c9c152507709e9
Steven Lieblich <steven.lieblich@gmail.com>
2016-08-05 12:27:27 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <boost/timer/timer.hpp>
+#include <cmath>
+
+int main() {
+    boost::timer::auto_cpu_timer t;
+
+    for (long i=0; i < 1000000000; ++i)
+        std:sqrt(123.456L); // burn some time
+
+    return 0;
+}
